c6a254f0901e4bb39f23b3969ed0eaf7fdaecd42
jungseok yu <9526yu@naver.com>
2025-11-07 17:52:33 +0900

    [FE] feat: gradle file 변경

--- mobile_watch/wear/build.gradle.kts
+++ mobile_watch/wear/build.gradle.kts
@@ -7,13 +7,13 @@ plugins {
 android {
     namespace = "com.example.secondbrain"
     compileSdk {
-        version = release(36)
+        version = release(35)
     }
 
     defaultConfig {
         applicationId = "com.example.secondbrain"
         minSdk = 30
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -29,13 +29,12 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
-    useLibrary("wear-sdk")
     buildFeatures {
         compose = true
     }
