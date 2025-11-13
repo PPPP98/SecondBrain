0bccb2fd13ede293cc77a453cbbfe3d8c0fb2656
박진호 <dodwn48@gmail.com>
2025-11-13 17:24:31 +0900

    [AI] feat: 개별 에이전트 선언

---
+++ knowledge-graph-service/app/agents/search_agent/models.py
@@ -0,0 +1,42 @@
+from langchain.chat_models import init_chat_model
+from app.core.config import get_settings
+
+# from app.schemas.agents import
+import os
+
+
+class Models:
+    """
+    ## LLM 모델 정의
+    - init : 환경변수 정의
+    - get_rewrite_model : 쿼리 재작성 에이전트
+    - get_related_check_model : content 내용 관련성 체크 에이전트
+    - get_result_model : 최종 응답 생성 에이전트
+    """
+
+    def __init__(self):
+        self.settings = get_settings()
+        os.environ["OPENAI_API_KEY"] = self.settings.openai_api_key
+        os.environ["OPENAI_API_BASE"] = self.settings.openai_base_url
+
+    def get_rewrite_model(self):
+        model = init_chat_model(
+            model=self.settings.search_agent_model,
+            temperature=self.settings.search_agent_temperature,
+        )
+        # structured_model = model.with_structured_output()
+        return model
+
+    def get_related_check_model(self):
+        model = init_chat_model(
+            model=self.settings.search_agent_model,
+            temperature=self.settings.search_agent_temperature,
+        )
+        return model
+
+    def get_result_model(self):
+        model = init_chat_model(
+            model=self.settings.search_agent_model,
+            temperature=self.settings.search_agent_temperature,
+        )
+        return model
